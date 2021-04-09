e5ce1847ec5514a5139de6400a7d2958ab9393ab
dongchunfeng <dcf_free@126.com>
2021-04-09 11:12:58 +0800

    1.增加子模块eureka作为注册中心 2.将单maven改造成maven父子项目

---
+++ eureka/src/main/java/com/online/EurekaApplication.java
@@ -0,0 +1,16 @@
+package com.online;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author MrDong
+ */
+@SpringBootApplication
+public class EurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+
+}
