d019782ff6f75848d41f8c6be3d9234f5cd324c7
dongchunfeng <dcf_free@126.com>
2021-04-09 11:56:47 +0800

    1.增加子模块system

--- eureka/src/main/java/com/online/EurekaApplication.java
+++ eureka/src/main/java/com/online/EurekaApplication.java
@@ -1,16 +1,27 @@
 package com.online;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.core.env.Environment;
 
 /**
  * @author MrDong
  */
 @SpringBootApplication
+@EnableEurekaServer
+@Slf4j
 public class EurekaApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(EurekaApplication.class, args);
+
+        SpringApplication app = new SpringApplication(EurekaApplication.class);
+        Environment environment = app.run(args).getEnvironment();
+        log.info("启动成功!");
+        log.info("Eureka: \thttp://127.0.0.1:{}", environment.getProperty("server.port"));
+
     }
+
 
 }
